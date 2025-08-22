21947c165bb07eeacdd1786d35303423a9692da3
Diganta Biswas <diganta_biswas@epam.com>
2025-08-23 02:59:17 +0530

    initial commit with basic setup

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='text-md w-full h-[10vh] p-5  flex justify-evenly items-center z-0 absolute'>
+        <div>works</div>
+        <div>skills</div>
+        <div className='text-2xl font-bold'>{"<//>"}</div>
+        <div>about</div>
+        <div>talk</div>
+    </nav>
+  )
+}
+
+export default Navbar
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Navbar from "./components/Navbar";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -20,8 +22,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
