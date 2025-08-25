2c68c0542b779c36a6d7526e7d07b67512c1a721
Diganta Biswas <diganta_biswas@epam.com>
2025-08-26 01:58:23 +0530

    skill section

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -1,8 +1,163 @@
+"use client"
+
 import React from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import Image from 'next/image'
 
 function Skills() {
+
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger)
+
+    gsap.to(".slider", {
+      x:"-100%",
+
+      scrollTrigger: {
+        trigger: ".slider",
+        start: "top 40%",
+        scrub: 2,
+        pin:true,
+        pinSpacing:true
+      }
+
+    });
+  })
   return (
-    <div className='w-full h-[100vh] flex justify-center items-center'>This is the skills page</div>
+    <>
+      <div className='w-[100vw] h-max flex justify-start items-start flex-col'>
+        <div className='quote w-full h-[100vh] flex justify-center items-center text-xl'>
+          Your skills are the silent power behind every bold creation
+        </div>
+
+        <div className='slider p-5 flex gap-5 justify-start items-center'>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/1.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/1-1.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/2.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/3.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/4.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/5.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/6.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/7.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/8.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/9.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/10.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/11.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/12.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/13.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/14.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/15.webp"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+          <div className='relative w-50 h-50'>
+            <Image
+              src="/logos/16.png"
+              fill
+              className="object-contain" 
+              alt="software log"/>
+          </div>
+        </div>
+
+        <div className='quote w-full h-[100vh] flex justify-center items-center text-xl'>
+          I believe in constantly upgrading my skills because growth never stops.
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/app/works/page.js
+++ src/app/works/page.js
@@ -1,10 +1,61 @@
 "use client"
 
+
+// clip-path: polygon(13% 6%, 87% 25%, 100% 100%, 0% 100%);
+// upperpart change
+
+// clip-path: polygon(0 0, 100% 0, 80% 94%, 19% 81%);
+// lowerpart change
+
+
 import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import Image from 'next/image'
 
 async function Works() {
+
+
+  useGSAP(() => {
+    const projects = gsap.utils.toArray(".project")
+    console.log(projects)
+  })
+
+
+
   return (
-    <div className='w-full h-[100vh] flex justify-center items-center'>This is the work page</div>
+    <>
+    {/* “Each work is proof that imagination can become code.” */}
+
+      <div className='w-[100vw] h-[100vh] relative'>
+
+        <div className='preview w-[350px] h-[300px] absolute top-[35vh] left-[60vw]'></div>
+
+        <div className='projects w-full h-full flex justify-start items-center ml-20'>
+          <div className='table w-[50%] h-[70%]'>
+
+            <div className='header flex justify-evenly items-center'>
+              <div className='w-full h-max p-4 text-left'>Projects</div>
+              <div className='w-full h-max p-4 text-left'>Year</div>
+            </div>
+            <div className='project border-t-1 flex justify-evenly items-center'>
+              <div className='w-full h-max p-4 text-left'>Confessionity</div>
+              <div className='w-full h-max p-4 text-left'>2023</div>
+              <div className='relative w-100 h-100'><Image
+                src="/images/hero-image.jpg"
+                alt="Profile Picture"
+                fill
+                className="image object-cover"
+              /></div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </>
   )
 }
 
